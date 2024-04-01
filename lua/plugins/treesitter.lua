cda8b412b72a8faa278edcfec53c61176b92efe4
ErikGrootoonk <hgrootoonk@proton.me>
2024-04-01 13:47:06 +0200

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,6 @@
       build = ":TSUpdate",
       config = function ()
         local configs = require("nvim-treesitter.configs")
-        require 'nvim-treesitter.install'.compilers = { "gcc" }
         configs.setup({
             ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html","yaml","terraform" },
             sync_install = false,
