918c3abc99441e7be58b29d6890e48568e19b6e4
Erik <erik.grootoonk@gmail.com>
2024-04-16 19:28:57 +0200

    clipboard tool configured

--- lua/options.lua
+++ lua/options.lua
@@ -55,3 +55,21 @@ vim.keymap.set('n', '<s-tab>', ':tabprev<cr>')
 
 -- set cursor line
 vim.opt.cursorline = true
+
+-- enable windows clipboard tool
+vim.opt.clipboard = "unnamedplus"
+
+if vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
+    },
+    cache_enabled = 0,
+  }
+end
