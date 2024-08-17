ea332d6f01fdadb53a54f67868410ff7f5c5c8e1
erik <hgrootoonk@proton.me>
2024-08-17 09:31:30 +0200

    tokynight and lualine Horizon theme added

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -13,7 +13,15 @@ return {
     name = "gruvbox",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme "gruvbox"
+    end
+  },
+  {
+    "ghifarit53/tokyonight-vim",
+    lazy = false,
+    name = "tokyonight",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme "tokyonight"
     end
   }
 }
