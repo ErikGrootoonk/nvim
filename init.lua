926801c037d89d49457c78a7be10439fddbf8309
erikg <erik.grootoonk@gmail.com>
2025-02-15 13:17:31 +0100

    mijn eigen nvim met hulp van typecraft

--- init.lua
+++ init.lua
@@ -1,52 +1,15 @@
-vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
-vim.g.mapleader = " "
-
--- bootstrap lazy and all plugins
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-
-if not vim.uv.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
-
 vim.opt.rtp:prepend(lazypath)
 
-local lazy_config = require "configs.lazy"
-
--- load plugins
-require("lazy").setup({
-  {
-    "NvChad/NvChad",
-    lazy = false,
-    branch = "v2.5",
-    import = "nvchad.plugins",
-  },
-
-  { import = "plugins" },
-}, lazy_config)
-
--- load theme
-dofile(vim.g.base46_cache .. "defaults")
-dofile(vim.g.base46_cache .. "statusline")
-
-require "options"
-require "nvchad.autocmds"
-
-vim.schedule(function()
-  require "mappings"
-end)
-
-
-vim.g.clipboard = {
-  name = 'win32yank-wsl',
-  copy = {
-    ['+'] = '/mnt/c/tools/win32yank.exe -i --crlf',
-    ['*'] = '/mnt/c/tools/win32yank.exe -i --crlf',
-  },
-  paste = {
-    ['+'] = '/mnt/c/tools/win32yank.exe -o --lf',
-    ['*'] = '/mnt/c/tools/win32yank.exe -o --lf',
-  },
-  cache_enabled = 0,
-}
-
+require("vim-options")
+require("lazy").setup("plugins")
